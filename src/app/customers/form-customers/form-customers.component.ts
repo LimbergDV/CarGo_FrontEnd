d4954ebecb2d480a65e13e2342f49dfcbb82f7e2
LimbergDV <limbergdelvalle@gmail.com>
2024-10-03 11:08:21 -0600

    modificando componentes del form user

--- src/app/customers/form-customers/form-customers.component.ts
+++ src/app/customers/form-customers/form-customers.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+// src/app/form-customers/form-customers.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../models/customer';// Ajusta la ruta
+import { CustomerService } from '../../../services/customer.service'; // Ajusta la ruta
 
 @Component({
   selector: 'app-form-customers',
   templateUrl: './form-customers.component.html',
-  styleUrl: './form-customers.component.css'
+  styleUrls: ['./form-customers.component.css']
 })
-export class FormCustomersComponent {
+export class FormCustomersComponent implements OnInit{
+  customer: Customer[] = []
+  custom: Customer = {
+    name: '',
+    last_names: '',
+    phone_number: '',
+    CURP: '',
+    number_licence: '',
+  }
 
+  constructor(private _customerService: CustomerService){}
+
+
+  ngOnInit():void{
+    this._customerService.createCustomer(this.custom).subscribe(
+      response =>{
+        this.custom = response
+        console.log(this.customer);
+        this.limpiarFormulario();
+
+      }
+    )
+  }
+  limpiarFormulario() {
+    this.custom = {
+      id_customer: 0,
+      name: '',
+      last_names: '',
+      phone_number: '',
+      CURP: '',
+      number_licence: '',
+      birthdate: ''
+    };
+  }
 }
