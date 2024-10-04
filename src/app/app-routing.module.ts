96db43a2c43932fce273eda614cc73aa4b1c3f46
LimbergDV <limbergdelvalle@gmail.com>
2024-10-03 21:59:21 -0600

    add: añadiendo componente de vista de carros y su enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewCustomerComponent } from './view-customer/view-customer.component';
+import { ViewCarsComponent } from './view-cars/view-cars.component';
 
 const routes: Routes = [
-  {path:'Clientes', component:ViewCustomerComponent}
+  {path:'Clientes', component:ViewCustomerComponent},
+  {path:'Autos', component: ViewCarsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CustomerTableComponent } from './customer-table/customer-table.componen
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ViewCustomerComponent } from './view-customer/view-customer.component';
+import { ViewCarsComponent } from './view-cars/view-cars.component';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { ViewCustomerComponent } from './view-customer/view-customer.component';
     FormCustomersComponent,
     CustomerTableComponent,
     ViewCustomerComponent,
+    ViewCarsComponent,
   ],
   imports: [
     BrowserModule,
