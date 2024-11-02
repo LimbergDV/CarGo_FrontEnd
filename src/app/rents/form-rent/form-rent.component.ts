398046f39e25ce5b8115ce63796274df043669a9
LimbergDV <limbergdelvalle@gmail.com>
2024-11-02 10:24:12 -0600

    add: adding styles in rents-form

--- src/app/rents/form-rent/form-rent.component.ts
+++ src/app/rents/form-rent/form-rent.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Customer } from '../../customers/models/customer';
 import { CustomerService } from '../../../services/customer.service';
 import { subscribeOn } from 'rxjs';
+import { Car } from '../../cars/models/car';
+import { RentService } from '../../../services/rent.service';
+import { CarService } from '../../../services/car.service';
+import { Rent } from '../models/rent';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form-rent',
@@ -10,16 +15,69 @@ import { subscribeOn } from 'rxjs';
 })
 export class FormRentComponent implements OnInit {
   customers: Customer [] = [];
+  cars: Car [] = [];
+  rent: Rent = {
+    id_customer: 0,
+    id_rent: 0,
+    id_car: 0,
+    start_date: new Date(),
+    end_date: new Date(),
+    total_price: 0,
+  };
 
-  constructor(private customerService: CustomerService){}
+  constructor(private customerService: CustomerService,
+    private rentService: RentService,
+    private carService: CarService){}
 
   ngOnInit(): void {
       this.loadCustomers();
+      this.loadCars();
   }
 
   loadCustomers(){
     this.customerService.getAllCustomers().subscribe(data =>{
       this.customers = data;
     });
+  }
+
+  loadCars(){
+    this.carService.getAllCars().subscribe(data =>{
+      this.cars = data;
+    })
+  }
+
+  rentCar(): void{
+    if (!this.rent.id_customer || !this.rent.id_car || !this.rent.start_date || !this.rent.end_date) {
+      Swal.fire('Error', 'All fields are required!', 'error');
+      return;
+    }
+
+    this.rentService.createRent(this.rent).subscribe(
+      (response) => {
+        console.log('Renta registrada:', response);
+        Swal.fire({
+          icon: 'success',
+          title: 'Car rented successfully!',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        this.resetForm();
+      },
+      (error) => {
+        console.error('Error registering rent:', error);
+        Swal.fire('Error', 'There was an error registering the rent.', 'error');
+      }
+    );
+  }
+
+  resetForm(): void {
+    this.rent = {
+      id_customer: 0,
+      id_rent: 0,
+      id_car: 0,
+      start_date: new Date(),
+      end_date: new Date(),
+      total_price: 0,
+    };
   }
 }
